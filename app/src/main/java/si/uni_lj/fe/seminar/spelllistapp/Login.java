df17d163dc615ebf1fdb454695b088124a9e1095
Maks Sladič <maks.sladic@gmail.com>
2023-03-31 10:04:54 +0200

    Initial commit

---
+++ app/src/main/java/si/uni_lj/fe/seminar/spelllistapp/Login.java
@@ -0,0 +1,28 @@
+package si.uni_lj.fe.seminar.spelllistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button=(Button) findViewById(R.id.LoginButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
