4bc7e7043c9eae01be6e6295fba9468af3426988
Maks Sladič <maks.sladic@gmail.com>
2023-04-05 14:41:08 +0200

    Spele prikazuje

--- app/src/main/java/si/uni_lj/fe/seminar/spelllistapp/AllSpells.java
+++ app/src/main/java/si/uni_lj/fe/seminar/spelllistapp/AllSpells.java
@@ -29,7 +29,9 @@ import java.util.List;
 
 public class AllSpells extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private RecyclerView recyclerView0,recyclerView1,recyclerView2,recyclerView3,recyclerView4,recyclerView5,recyclerView6,recyclerView7,recyclerView8,recyclerView9;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,125 +40,30 @@ public class AllSpells extends AppCompatActivity {
         String url = "http://10.0.2.2/spell_list_app/spells/";
 
 
-        for(int i=0;i<10;i++)
-        {
-            String CallURL = "";
-            switch (i){
-                case 0:
-                    recyclerView = findViewById(R.id.recyclerview_spells_cantrips);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(AllSpells.this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    CustomAdapter customadapter= new CustomAdapter(AllSpells.this, GetSpells);
-                    recyclerView.setAdapter(customadapter);
-                    break;
-                case 1:
-                    recyclerView = findViewById(R.id.recyclerview_spells_first);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 2:
-                    recyclerView = findViewById(R.id.recyclerview_spells_second);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 3:
-                    recyclerView = findViewById(R.id.recyclerview_spells_third);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 4:
-                    recyclerView = findViewById(R.id.recyclerview_spells_fourth);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 5:
-                    recyclerView = findViewById(R.id.recyclerview_spells_fifth);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 6:
-                    recyclerView = findViewById(R.id.recyclerview_spells_sixth);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 7:
-                    recyclerView = findViewById(R.id.recyclerview_spells_seventh);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 8:
-                    recyclerView = findViewById(R.id.recyclerview_spells_eighth);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-                case 9:
-                    recyclerView = findViewById(R.id.recyclerview_spells_ninth);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                    CallURL = url + i;
-                    new GetSpells().execute(CallURL);
-                    break;
-            }
-        }
+        String cantrips = getIntent().getStringExtra("cantrips");
+        String firstLevel = getIntent().getStringExtra("firstLevel");
+        String secondLevel = getIntent().getStringExtra("secondLevel");
+//        String cantrips = getIntent().getStringExtra("cantrips");
+//        String cantrips = getIntent().getStringExtra("cantrips");
+//        String cantrips = getIntent().getStringExtra("cantrips");
+//        String cantrips = getIntent().getStringExtra("cantrips");
+//        String cantrips = getIntent().getStringExtra("cantrips");
+        String ninthLevel = getIntent().getStringExtra("ninthLevel");
+
+
+        CustomAdapter customadapter0 = new CustomAdapter(this,cantrips);
+        CustomAdapter customadapter1 = new CustomAdapter(this,firstLevel);
+        recyclerView0 = findViewById(R.id.recyclerview_spells_cantrips);
+        recyclerView1 = findViewById(R.id.recyclerview_spells_first);
+
+        recyclerView0.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView1.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView0.setAdapter(customadapter0);
+        recyclerView1.setAdapter(customadapter1);
+
 
     }
-    public class GetSpells extends AsyncTask<String, Void, String>
-    {
-
-        @Override
-        protected String doInBackground(String... strings) {
-            HttpURLConnection getspells;
-            try {
-                URL url = new URL(strings[0]);
-                getspells = (HttpURLConnection) url.openConnection();
-                getspells.setRequestMethod("GET");
-                getspells.setReadTimeout(5000);
-                getspells.setConnectTimeout(10000);
-                int status = getspells.getResponseCode();
-                if (status == 200){
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(getspells.getInputStream()));
-                    StringBuilder response = new StringBuilder();
-                    String odgovor;
-                    while((odgovor = reader.readLine()) != null){
-                        response.append(odgovor);
-                    }
-
-                    return response.toString();
-                }
-            }
-            catch (Exception e) {
-                Log.d("error", String.valueOf(e));
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String response) {
-            Log.d("Response", String.valueOf(response));
-
-            try {
-                JSONArray spells = new JSONArray(response);
-                Log.d("Spells: ", String.valueOf(spells));
-                for (int i = 0; i<spells.length();i++){
-                    JSONObject object = spells.getJSONObject(i);
-                    Log.d("Spell Name",object.getString("SpellName"));
-
-
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     public class SpellInfo extends RecyclerView.ViewHolder {
         public TextView SpellName, CastingTime, Components, SpellSchool, Ritual, Concentration;
         public SpellInfo(@NonNull View itemView) {
@@ -174,6 +81,8 @@ public class AllSpells extends AppCompatActivity {
         private Context context;
         private String string;
 
+        private List list;
+
         public CustomAdapter(Context context, String string) {
             this.context = context;
             this.string = string;
@@ -189,9 +98,7 @@ public class AllSpells extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull SpellInfo holder, int position) {
             JSONArray spells = null;
-            List list;
             try {
-                Log.d("Tukej ","tukej");
                 spells = new JSONArray(string);
                 list = new ArrayList<>();
                 for (int i = 0; i < spells.length(); i++) {
@@ -210,7 +117,16 @@ public class AllSpells extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return 0;
+            if (string == null) {
+                return 0;
+            }
+            try {
+                JSONArray spells = new JSONArray(string);
+                return spells.length();
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+//            return list.size();
         }
     }
 
--- app/src/main/java/si/uni_lj/fe/seminar/spelllistapp/CharacterSelection.java
+++ app/src/main/java/si/uni_lj/fe/seminar/spelllistapp/CharacterSelection.java
@@ -1,18 +1,45 @@
 package si.uni_lj.fe.seminar.spelllistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class CharacterSelection extends AppCompatActivity {
+
+    private String cantrips;
+    private String firstLevel;
+    private String secondLevel;
+    private String thirdLevel;
+    private String fourthLevel;
+    private String fifthLevel;
+    private String sixthLevel;
+    private String seventhLevel;
+    private String eighthLevel;
+    private String ninthLevel;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
+        String url = "http://10.0.2.2/spell_list_app/spells/";
+
 
         Button button=(Button) findViewById(R.id.LogoutButton);
 
@@ -30,8 +57,115 @@ public class CharacterSelection extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent= new Intent(CharacterSelection.this,AllSpells.class);
+                intent.putExtra("cantrips", cantrips);
+                intent.putExtra("firstLevel", firstLevel);
+                intent.putExtra("secondLevel", secondLevel);
+                intent.putExtra("thirdLevel", thirdLevel);
+                intent.putExtra("fourthLevel", fourthLevel);
+                intent.putExtra("fifthLevel", fifthLevel);
+                intent.putExtra("sixthLevel", sixthLevel);
+                intent.putExtra("seventhLevel", seventhLevel);
+                intent.putExtra("eighthLevel", eighthLevel);
+                intent.putExtra("ninthLevel", ninthLevel);
                 startActivity(intent);
             }
         });
+
+        for (int i = 0; i < 10; i++) {
+            String CallURL = "";
+
+            CallURL = url + i;
+            new GetSpells(this, i).execute(CallURL);
+
+        }
     }
+
+    public class GetSpells extends AsyncTask<String, Void, String>
+    {
+        private int level;
+        private CharacterSelection activity;
+
+        public GetSpells(CharacterSelection activity, int level) {
+            this.activity = activity;
+            this.level = level;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection getspells;
+            try {
+                URL url = new URL(strings[0]);
+                getspells = (HttpURLConnection) url.openConnection();
+                getspells.setRequestMethod("GET");
+                getspells.setReadTimeout(5000);
+                getspells.setConnectTimeout(10000);
+                int status = getspells.getResponseCode();
+                if (status == 200){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(getspells.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String odgovor;
+                    while((odgovor = reader.readLine()) != null){
+                        response.append(odgovor);
+                    }
+
+                    return response.toString();
+                }
+            }
+            catch (Exception e) {
+                Log.d("error", String.valueOf(e));
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            Log.d("Response", String.valueOf(response));
+
+            try {
+                JSONArray spells = new JSONArray(response);
+                Log.d("Spells: ", String.valueOf(spells));
+
+                for (int i = 0; i<spells.length();i++){
+                    JSONObject object = spells.getJSONObject(i);
+                    Log.d("Spell Name",object.getString("SpellName"));
+                }
+
+                switch (level) {
+                    case 0:
+                        activity.cantrips = String.valueOf(spells);
+                        break;
+                    case 1:
+                        activity.firstLevel = String.valueOf(spells);
+                        break;
+                    case 2:
+                        activity.secondLevel = String.valueOf(spells);
+                        break;
+                    case 3:
+                        activity.thirdLevel = String.valueOf(spells);
+                        break;
+                    case 4:
+                        activity.fourthLevel = String.valueOf(spells);
+                        break;
+                    case 5:
+                        activity.fifthLevel = String.valueOf(spells);
+                        break;
+                    case 6:
+                        activity.sixthLevel = String.valueOf(spells);
+                        break;
+                    case 7:
+                        activity.seventhLevel = String.valueOf(spells);
+                        break;
+                    case 8:
+                        activity.eighthLevel = String.valueOf(spells);
+                        break;
+                    case 9:
+                        activity.ninthLevel = String.valueOf(spells);
+                        break;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
